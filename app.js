8cd91a66d9a4ab81ab09cdc4a9851f398a5eb3dd
Toni <toni@Jess-MacBook-Pro.local>
2020-09-15 17:44:21 -0400

    updated jquery to include image from spreadsheet

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ let sheetAsJson =
     'https://spreadsheets.google.com/feeds/list/1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U/od6/public/values?alt=json';
     
 const render = (projectArr) => {
-     
+    const $div = $('<div>').addClass('projects').text(projectArr[0].title);
+    $('#projects-div').append($div);
+    const $img = $('<img>').attr('src', projectArr[0].image)
+    $('#projects-div').append($img)    
+    console.log(projectArr[0].title)
     console.log("this is projectArr", projectArr)
 }
 
 $.ajax({url:sheetAsJson})
     .then(data => {
         const projects = data.feed.entry.map(project => {
+            console.log(project)
             return{
                 title: project.gsx$title.$t, 
-                image: project.gsx$image$t, 
+                image: project.gsx$image.$t, 
                 description: project.gsx$description.$t
             }
         })
         render(projects)
     })
 
-    const $div = $('<div>').addClass('projects').text('projects')
-    $('#projects-div').append($div)
+    // const $div = $('<div>').addClass('projects').text('projects')
+    // $('#projects-div').append($div)
