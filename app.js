ea839029cbb77ae9608e46054a0801ce9f5b5837
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-03 21:35:38 -0500

    added email functionality

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
 let sheetAsJson =
-    'https://spreadsheets.google.com/feeds/list/1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U/od6/public/values?alt=json';
-    
-$.ajax({url: sheetAsJson})
-    .then(data => {
-        const projects = data.feed.entry.forEach(project => {
-            const $renderedProj = $('<h3>')
-							.text(project.gsx$title.$t)
-							.addClass('title');
-            const $renderedImg = $('<img>')
-				.attr('src', project.gsx$image.$t)
-					.addClass('project');
-            const $renderedDescription = $('<p>')
-				.text(project.gsx$description.$t)
-                    .addClass('description');
-            const $renderedLink = $('<a>')
-                .attr('href', project.gsx$link.$t)
-                    .addClass('link');
-         $('#projects-div').append($renderedProj, $renderedImg, $renderedDescription, $renderedLink);
-        })
-})
+	'https://spreadsheets.google.com/feeds/list/1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U/od6/public/values?alt=json';
+
+$.ajax({ url: sheetAsJson }).then((data) => {
+	const projects = data.feed.entry.forEach((project) => {
+		const $renderedProj = $('<h3>')
+			.text(project.gsx$title.$t)
+			.addClass('title');
+		const $renderedImg = $('<img>')
+			.attr('src', project.gsx$image.$t)
+			.addClass('project')
+		const $renderedDescription = $('<p>')
+			.text(project.gsx$description.$t)
+			.addClass('description');
+		const $renderedLink = $('<a>')
+			.attr('href', project.gsx$link.$t)
+			.addClass('link');
+		$('#projects-div').append(
+			$renderedProj,
+			$renderedImg,
+			$renderedDescription,
+			$renderedLink
+		);
+	});
+});
 
 $('.submit-button').on('click', () => {
-    console.log('form submit clicked');
+    console.log('form submit clicked')
 });
 
 //used stackoverflow for help with scroll function
 $('.top-button').on('click', () => {
-    $(window).scrollTop(0);
+	$(window).scrollTop(0);
 });
