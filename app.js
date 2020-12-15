acd1930a1d2b9657f98a97dba7742abad48de7c3
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-15 18:15:57 -0500

    fixed broken links

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 let sheetAsJson =
 	'https://spreadsheets.google.com/feeds/list/1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U/od6/public/values?alt=json';
-
-$.ajax({ url: sheetAsJson }).then((data) => {
+// let sheetAsJson =
+// 	'https://spreadsheets.google.com/feeds/cells/1d9tAR6r3q7A2SO38rbpBBUalOPZsBfiCq7v8kriYtbM/1/public/full?alt=json';
+https: $.ajax({ url: sheetAsJson }).then((data) => {
 	const projects = data.feed.entry.forEach((project) => {
+		console.log(project);
 		const $renderedProj = $('<h3>')
 			.text(project.gsx$title.$t)
 			.addClass('title');
 		const $renderedImg = $('<img>')
 			.attr('src', project.gsx$image.$t)
-			.addClass('project')
+			.addClass('project');
 		const $renderedDescription = $('<p>')
-			.text(project.gsx$description.$t)
+			.text(project.gsx$skillsused.$t)
 			.addClass('description');
-		const $renderedLink = $('<a>')
-			.attr('href', project.gsx$link.$t)
-			.addClass('link');
+		// const $renderedLink = $('<a>')
+		// 	.attr('href', project.gsx$link.$t)
+		// 	.addClass('link');
 		$('#projects-div').append(
 			$renderedProj,
 			$renderedImg,
-			$renderedDescription,
-			$renderedLink
+			$renderedDescription
+			// $renderedLink
 		);
 	});
 });
 
 $('.submit-button').on('click', () => {
-	console.log('form submit clicked')
+	console.log('form submit clicked');
 });
 //used stackoverflow for help with scroll function
 $('.top-button').on('click', () => {
