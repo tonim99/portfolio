c953c8e99eed6936e2a1a5fc39122a299e73b614
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-19 04:21:59 -0500

    updated grid x2

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let sheetAsJson =
 	'https://spreadsheets.google.com/feeds/list/1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U/od6/public/values?alt=json';
-// let sheetAsJson =
-// 	'https://spreadsheets.google.com/feeds/cells/1d9tAR6r3q7A2SO38rbpBBUalOPZsBfiCq7v8kriYtbM/1/public/full?alt=json';
+
 https: $.ajax({ url: sheetAsJson }).then((data) => {
 	const projects = data.feed.entry.forEach((project) => {
 		console.log(project);
@@ -29,7 +28,7 @@ https: $.ajax({ url: sheetAsJson }).then((data) => {
 $('.submit-button').on('click', () => {
 	console.log('form submit clicked');
 });
-//used stackoverflow for help with scroll function
+
 $('.top-button').on('click', () => {
 	$(window).scrollTop(0);
 });
